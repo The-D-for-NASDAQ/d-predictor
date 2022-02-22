04bf9becb55faece670c167f008b104fae9c2143
Yevheniy <yevheniy2407@gmail.com>
2022-02-22 18:56:59 +0200

    add checking last prediction

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ class bcolors:
     ENDC = '\033[0m'
 
 
+last_price = 0
+last_p_index = None
+
+new_price = 0
+
+
 predict_x_by_minutes = 10
 
 d_num_layers = 6  # Price, Ordered volume, Filled volume, Canceled volume, Pending volume, Time index
@@ -49,11 +55,12 @@ def load_data(to_date):
 
 
 def validate_and_convert_data(data):
+    global new_price
+
     price_entries = data[0, highest_bid_position-1:highest_bid_position+1]
     price_difference = price_entries[0] - price_entries[1]
 
-    print('Current buying price: ' + str(price_entries[1, -1]))
-    print('Current selling price: ' + str(price_entries[0, -1]))
+    new_price = price_entries[1, -1]
 
     if np.any(price_difference < 0) or np.any(price_difference > 0.5):
         return
@@ -72,6 +79,8 @@ def validate_and_convert_data(data):
 
 
 def main(date_to_process):
+    global last_price, last_p_index
+
     begin_time = datetime.now()
 
     data = load_data(date_to_process)
@@ -86,6 +95,23 @@ def main(date_to_process):
 
     prediction = model.predict(converted_data)[0]
     p_index = np.argmax(prediction)
+
+    if last_price and last_p_index is not None:
+        price_difference = new_price - last_price
+        if last_p_index == 0:
+            if price_difference < 0:
+                print(bcolors.UPGREEN + 'Down ▼ prediction was correct' + bcolors.ENDC)
+            else:
+                print(bcolors.DOWNRED + 'Down ▼ prediction was incorrect' + bcolors.ENDC)
+
+        if last_p_index == 2:
+            if price_difference > 0:
+                print(bcolors.UPGREEN + 'Up ▲ prediction was correct' + bcolors.ENDC)
+            else:
+                print(bcolors.DOWNRED + 'Up ▲ prediction was incorrect' + bcolors.ENDC)
+
+    last_price = new_price
+    last_p_index = p_index
 
     predicted_direction = 'None'
     if p_index == 0:
