ee2c8defcccf49db3d2aaa49cce0a3ddd53e32ec
Yevheniy <yevheniy2407@gmail.com>
2022-02-22 18:56:07 +0200

    fix metrics

--- metrics.py
+++ metrics.py
@@ -9,21 +9,22 @@ def get_accuracy(y_test, predictions):
 
     true_answers = len(np.where(p_indexes == y_indexes)[0])
 
-    y_sub_1 = len(np.where(y_test[:, 0] == 1)[0])
-    y_0 = len(np.where(y_test[:, 1] == 1)[0])
-    y_1 = len(np.where(y_test[:, 2] == 1)[0])
+    y_sub_1 = np.where(y_test[:, 0] == 1)[0]
+    y_0 = np.where(y_test[:, 1] == 1)[0]
+    y_1 = np.where(y_test[:, 2] == 1)[0]
 
-    true_sub_1_answers = len(
-        np.intersect1d(np.where(np.argmax(y_test, axis=1) == 0)[0], np.where(np.argmax(predictions, axis=1) == 0)[0]))
-    true_0_answers = len(
-        np.intersect1d(np.where(np.argmax(y_test, axis=1) == 1)[0], np.where(np.argmax(predictions, axis=1) == 1)[0]))
-    true_1_answers = len(
-        np.intersect1d(np.where(np.argmax(y_test, axis=1) == 2)[0], np.where(np.argmax(predictions, axis=1) == 2)[0]))
+    _sub_1_answers = np.where(np.argmax(predictions, axis=1) == 0)[0]
+    _0_answers = np.where(np.argmax(predictions, axis=1) == 1)[0]
+    _1_answers = np.where(np.argmax(predictions, axis=1) == 2)[0]
 
-    print('Total true answers: ' + str(math.floor(true_answers * 100 / len(predictions))) + '%')
-    print('-1 true answers: ' + str(math.floor(true_sub_1_answers * 100 / y_sub_1)) + '%')
-    print('0 true answers: ' + str(math.floor(true_0_answers * 100 / y_0)) + '%')
-    print('1 true answers: ' + str(math.floor(true_1_answers * 100 / y_1)) + '%')
+    true_sub_1_answers = len(np.intersect1d(_sub_1_answers, y_sub_1))
+    true_0_answers = len(np.intersect1d(_0_answers, y_0))
+    true_1_answers = len(np.intersect1d(_1_answers, y_1))
+
+    print('Total true answers: ' + str(math.floor(true_answers / len(predictions) * 100)) + '%')
+    print('-1 true answers: ' + str(math.floor(true_sub_1_answers / len(_sub_1_answers) * 100)) + '%')
+    print('0 true answers: ' + str(math.floor(true_0_answers / len(_0_answers) * 100)) + '%')
+    print('1 true answers: ' + str(math.floor(true_1_answers / len(_1_answers) * 100)) + '%')
 
 
 def get_mean_absolute_error(predictions, y_test):
